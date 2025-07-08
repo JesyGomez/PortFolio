46216d118f8772b235f3719a4611383707609d18
julianycia.m@gmail.com <julianycia.m@gmail.com>
2025-07-08 11:36:16 -0300

    Corregida ubicación de la propiedad 'base' en vite.config.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/PortFolio/',
+  // ------------------------------------------------------------------
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), 
+      '@': path.resolve(__dirname, './src'),
     },
-      base: '/PortFolio/',
   },
 })
